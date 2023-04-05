22ac8123dfdaadd985633ef1dca3e3447b506d5a
nahaktarun <tarunnahak25@gmail.com>
2023-04-05 15:35:03 +0530

    added the hoc as the wrapper of the all the components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,7 @@ import {services} from '../constants';
 import {fadeIn, textVariant} from '../utils/motion';
 
 
+import  {SectionWrapper} from '../hoc';
 const ServiceCard = ({index, title, icon}) =>{
   return (
     <Tilt className="xs:w-[250px] w-full">
@@ -54,4 +55,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about");
